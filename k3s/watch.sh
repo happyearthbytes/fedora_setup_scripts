fdde8a430139fba6f83d8de942d37fdbcaaf9984
joe.dipilato <joe.dipilato@fedora.pc>
2022-12-10 23:55:29 -0500

    updates for helm3

--- k3s/watch.sh
+++ k3s/watch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-NAMESPACE=""
-# NAMESPACE="-A"
+# NAMESPACE=""
+NAMESPACE="-A"
 # NAMESPACE="-n proxy"
 COMMAND="kubectl get pods,deployments,services,ingress,daemonset -o wide ${NAMESPACE} | expand | cut -c-\${COLUMNS} | sed '/^$/d'"
 watch -t -n1 --color -d ${COMMAND}
