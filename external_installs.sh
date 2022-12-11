886f5a3521f6434013aa4f695baf7ae6b7e4ddb5
joe.dipilato <joe.dipilato@fedora.pc>
2022-12-10 22:53:50 -0500

    add helm

--- external_installs.sh
+++ external_installs.sh
@@ -6,10 +6,29 @@ dnf_command() {
     rc=$?
     if [[ $rc == 0 ]]; then echo "True"; else echo "False"; fi
 }
+
+linux_command() {
+    if ! command -v $1 &> /dev/null
+    then
+        echo "False"
+    else
+        echo "True"
+    fi
+}
+
+can_run() { linux_command "$1"; }
 is_found() { dnf_command "list" "$1"; }
 can_update() { dnf_command "list --upgrades" "$1"; }
 is_installed() { dnf_command "list --installed" "$1"; }
-should_install() {
+should_install_command() {
+    arg=$1
+    if [[ $(can_run $arg) == "False" ]]; then
+        echo "True"
+    else
+        echo "$arg already installed" 1>&2
+    fi
+}
+should_install_dnf() {
     arg=$1
     if [[ $(is_found $arg) == "False" || \
     $(is_installed $arg) == "False" || \
@@ -20,20 +39,21 @@ should_install() {
     fi
 }
 
-if [[ $(should_install code) == "True" ]]; then
+if [[ $(should_install_dnf code) == "True" ]]; then
     sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
     sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
     dnf check-update
 fi
-if [[ $(should_install k3s-selinux) == "True" ]]; then
+if [[ $(should_install_command k3s) == "True" ]]; then
     sudo mkdir -p /etc/rancher/k3s/
     sudo chmod -R 777 /etc/rancher
     cp k3s/config.yaml /etc/rancher/k3s/
     curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--disable=traefik" INSTALL_K3S_SKIP_ENABLE=true sh -s - server
 fi
-if [[ $(should_install helm) == "True" ]]; then
+if [[ $(should_install_command helm) == "True" ]]; then
     # curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > install-helm.sh
-    curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash -s -
+    echo "----------------"
+    # curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash -s -
     # u+x install-helm.sh
     # https://gist.github.com/icebob/958b6aeb0703dc24f436ee8945f0794f
     # https://medium.com/@k.sahanshah/setup-helm-in-a-k3s-6376acf00b18
@@ -41,7 +61,7 @@ if [[ $(should_install helm) == "True" ]]; then
     # https://doc.traefik.io/traefik/getting-started/quick-start-with-kubernetes/
     # https://blog.zachinachshon.com/traefik-ingress/
 fi
-if [[ $(should_install rpmfusion-free-release) == "True" ]]; then
+if [[ $(should_install_dnf rpmfusion-free-release) == "True" ]]; then
     sudo dnf -y install \
         https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 fi
