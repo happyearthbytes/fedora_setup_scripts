ce62202aebbe0f338ce5da5aa34257d6054ffdd3
joe.dipilato <joe.dipilato@fedora.pc>
2022-12-11 00:37:50 -0500

    traefik routing

--- traefik/start.sh
+++ traefik/start.sh
@@ -6,6 +6,8 @@ helm repo add traefik https://traefik.github.io/charts
 helm repo update
 helm install traefik traefik/traefik --namespace=traefik
 
+PWHASH=$(htpasswd -n ${USER} | openssl base64)
+sed 's/\(  users: \).*/\1'${PWHASH}'/' secret.yml.in > secret.yml
 
 # TRAEFIK_IP=$(kubectl get svc traefik-web-service -o custom-columns=EXTERNAL_IP:.status.loadBalancer.ingress[0].ip)
 # echo "${TRAEFIK_IP} traefik.themachine.pc traefik > /etc/hosts"
