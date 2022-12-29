f911d83c166f20b31c1c839b44e472c92d76572b
joe.dipilato <joe.dipilato@fedora.pc>
2022-12-28 19:12:35 -0500

    cleanup for new install

--- external_installs.sh
+++ external_installs.sh
@@ -59,182 +59,171 @@ service_running() {
         echo "False"
     fi
 }
-if [[ $(should_install_dnf rpmfusion-free-release) == "True" ]]; then
-    sudo dnf -y install \
-        https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+
+install_common() {
+    if [[ $(should_install_dnf rpmfusion-free-release) == "True" ]]; then
+        sudo dnf -y install \
+            https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+    fi
+    if [[ $(should_install_dnf code) == "True" ]]; then
+        sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+        sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+        dnf check-update
+        sudo dnf install -y code
+    fi
+}
+
+install_k3s() {
+    if [[ $(should_install_command helm) == "True" ]]; then
+        curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+    fi
+    if [[ $(should_install_command k3s) == "True" ]]; then
+        sudo mkdir -p /etc/rancher/k3s/
+        sudo chmod -R 777 /etc/rancher
+        cp k3s/config.yaml /etc/rancher/k3s/
+        sudo swapoff -a
+        curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--disable=traefik" INSTALL_K3S_SKIP_ENABLE=true INSTALL_K3S_SKIP_START=true sh -s - server
+        # INSTALL_K3S_VERSION="v1.23.7+k3s1" sh
+    fi
+}
+
+start_k3s() {
+    if [[ $(service_running k3s) == "False" ]]; then
+        # Set defaults
+        sudo mkdir -p /var/lib/rancher/k3s/agent/etc/containerd/
+        sudo mkdir -p /etc/containerd/
+        containerd config default | sudo tee /var/lib/rancher/k3s/agent/etc/containerd/config.toml 2>&1 > /dev/null
+        containerd config default | sudo tee /etc/containerd/config.toml 2>&1 > /dev/null
+        # Copy the template
+        sudo cp k3s/config.toml.tmpl /var/lib/rancher/k3s/agent/etc/containerd/
+        sudo cp k3s/config.toml.tmpl /etc/containerd/
+
+        sudo cp k3s/registries.yaml /etc/rancher/k3s/
+
+        sudo systemctl start k3s
+        sudo systemctl enable k3s
+        sleep 1
+        mkdir -p ~/.kube
+        kubectl config view --raw > ~/.kube/config
+        chmod 600 ~/.kube/config
+    fi
+}
+
+install_nvidia() {
+    if [[ $(should_install_dnf nvidia-driver) == "True" ]]; then
+        # sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
+
+        # https://docs.nvidia.com/datacenter/tesla/tesla-installation-notes/index.html
+        sudo dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/fedora36/x86_64/cuda-fedora36.repo
+        sudo dnf clean all
+        sudo dnf -y module install --allowerasing nvidia-driver:latest-dkms
+        sudo dnf -y install cuda
+        add_bashrc "export PATH=/usr/local/cuda-12.0/bin${PATH:+:${PATH}}"
+
+        # Note that the fedora37 repository does not exist at the time of writing this
+        # sudo rpm --erase gpg-pubkey-7fa2af80*
+        # source /etc/os-release
+        # distro=${ID}${VERSION_ID}
+        # sudo dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/$distro/x86_64/cuda-$distro.repo
+        # sudo dnf clean expire-cache
+        # sudo dnf --disablerepo="rpmfusion-nonfree*" install cuda
+
+        sudo systemctl enable nvidia-persistenced
+        sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+        sudo dracut --regenerate-all --force
+        sudo reboot
+    fi
+}
+
+start_nvidia_k3s() {
+    # Cuda test
+    # if [[ $(should_install_dnf nvidia-driver "cuda test") == "True" ]]; then
+    #     nvidia-smi
+    #     which nvidia-container-runtime
+    #     sudo dnf install -y freeglut-devel libX11-devel libXi-devel libXmu-devel make mesa-libGLU-devel freeimage-devel
+    #     git clone https://github.com/NVIDIA/cuda-samples.git
+    #     make -C cuda-samples/Samples/1_Utilities/deviceQuery
+    #     make -C cuda-samples/Samples/1_Utilities/bandwidthTest
+    #     cuda-samples/Samples/1_Utilities/deviceQuery/deviceQuery
+    #     cuda-samples/Samples/1_Utilities/bandwidthTest/bandwidthTest
+    # fi
+    if [[ $(should_install_dnf nvidia-container-toolkit) == "True" ]]; then
+        # https://github.com/NVIDIA/k8s-device-plugin#quick-start
+        # https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html
+
+        distribution=$(. /etc/os-release;echo centos8) \
+        && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.repo | sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
+        sudo dnf clean expire-cache
+        sudo dnf install -y nvidia-container-toolkit
+        distribution=$(. /etc/os-release;echo centos8) \
+            && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.repo | sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
+        # cat  /usr/share/containers/oci/hooks.d/oci-nvidia-hook.json
+
+        sudo dnf install -y nvidia-container-runtime nvidia-modprobe
+
+        # https://dischord.org/2022/06/14/rke2-and-nvidia-gpus-with-the-grid-operator/
+        # https://dischord.org/2022/05/16/k3s-rke2-and-gpu-pci-passthrough/
+
+        # grep "nvidia-container-runtime" /var/lib/rancher/rke2/agent/etc/containerd/config.toml &>/dev/null && info "GPU containerd changes already applied" && exit 0
+        # awk '1;/plugins.cri.containerd]/{print "  default_runtime_name = \"nvidia-container-runtime\""}' /var/lib/rancher/rke2/agent/etc/containerd/config.toml > /var/lib/rancher/rke2/agent/etc/containerd/config.toml.tmpl
+        # echo -e '\n[plugins.linux]\n  runtime = "nvidia-container-runtime"' >> /var/lib/rancher/rke2/agent/etc/containerd/config.toml.tmpl
+        # echo -e '\n[plugins.cri.containerd.runtimes.nvidia-container-runtime]\n  runtime_type = "io.containerd.runc.v2"\n  [plugins.cri.containerd.runtimes.nvidia-container-runtime.options]\n    BinaryName = "nvidia-container-runtime"' >> /var/lib/rancher/rke2/agent/etc/containerd/config.toml.tmpl
+
+        # DOCKER_REGISTRY_URL=$(cat defaults.json | jq -er ".registries.docker.url")
+        # sed -i "s/REGISTRY_PLACEHOLDER/${DOCKER_REGISTRY_URL}/g" ./Infra_Installer/gpu_plugin/nvidia-device-plugin.yaml
+        # kubectl apply -f ./Infra_Installer/gpu_plugin/nvidia-device-plugin.yaml
+        # kubectl -n kube-system rollout restart daemonset nvidia-device-plugin-daemonset
+
+        # root@ubuntu:~# cat /var/lib/rancher/k3s/agent/etc/containerd/config.toml | grep -i nvidia
+        # [plugins.cri.containerd.runtimes."nvidia"]
+        # [plugins.cri.containerd.runtimes."nvidia".options]
+        #   BinaryName = "/usr/bin/nvidia-container-runtime"
+
+        # kubectl get runtimeclass
+        # kubectl describe node | grep gpu
+    fi
+
+    if [[ $(service_running k3s) == "False" ]]; then
+        kubectl apply -f k3s/nvidia-namespace.yaml
+
+        kubectl create cm -n nvidia-device-plugin nvidia-plugin-configs \
+            --from-file=config=k3s/nvidia-config.yaml
+
+        kubectl apply -f k3s/nvidia-device-plugin.yml
+
+        label_filter="owner=joe"
+        node_name=$(kubectl get nodes -l ${label_filter} --no-headers=true -o custom-columns=NAME:.metadata.name)
+        kubectl label node --overwrite ${node_name} nvidia.com/device-plugin.config=nvidia-plugin-configs
+
+        # sleep 5
+        # kubectl -n kube-system create serviceaccount tiller
+        # kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+        # helm init --service-account tiller
+
+        #    --write-kubeconfig value, -o value         (client) Write kubeconfig for admin client to this file [$K3S_KUBECONFIG_OUTPUT]
+        #    --kube-apiserver-arg value                 (flags) Customized flag for kube-apiserver process
+        #    --node-name value                          (agent/node) Node name [$K3S_NODE_NAME]
+        #    --container-runtime-endpoint value         (agent/runtime) Disable embedded containerd and use the CRI socket at the given path; when used with --docker this sets the docker socket path
+        #    --private-registry value                   (agent/runtime) Private registry configuration file (default: "/etc/rancher/k3s/registries.yaml")
+
+        # https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html#step-0-pre-requisites
+    fi
+}
+
+
+
+######
+# main
+######
+
+INSTALL_NVIDIA=0
+
+
+install_common
+install_k3s
+start_k3s
+
+if [[ $INSTALL_NVIDIA == "1" ]]; then
+    install_nvidia
+    start_nvidia_k3s
 fi
-if [[ $(should_install_dnf code vscode) == "True" ]]; then
-    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-    sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-    dnf check-update
-fi
-if [[ $(should_install_dnf nvidia-driver) == "True" ]]; then
-    # sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
-
-    # https://docs.nvidia.com/datacenter/tesla/tesla-installation-notes/index.html
-    sudo dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/fedora36/x86_64/cuda-fedora36.repo
-    sudo dnf clean all
-    sudo dnf -y module install --allowerasing nvidia-driver:latest-dkms
-    sudo dnf -y install cuda
-    add_bashrc "export PATH=/usr/local/cuda-12.0/bin${PATH:+:${PATH}}"
-
-    # Note that the fedora37 repository does not exist at the time of writing this
-    # sudo rpm --erase gpg-pubkey-7fa2af80*
-    # source /etc/os-release
-    # distro=${ID}${VERSION_ID}
-    # sudo dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/$distro/x86_64/cuda-$distro.repo
-    # sudo dnf clean expire-cache
-    # sudo dnf --disablerepo="rpmfusion-nonfree*" install cuda
-
-    sudo systemctl enable nvidia-persistenced
-    sudo grub2-mkconfig -o /boot/grub2/grub.cfg
-    sudo dracut --regenerate-all --force
-    sudo reboot
-fi
-# Cuda test
-# if [[ $(should_install_dnf nvidia-driver "cuda test") == "True" ]]; then
-#     nvidia-smi
-#     which nvidia-container-runtime
-#     sudo dnf install -y freeglut-devel libX11-devel libXi-devel libXmu-devel make mesa-libGLU-devel freeimage-devel
-#     git clone https://github.com/NVIDIA/cuda-samples.git
-#     make -C cuda-samples/Samples/1_Utilities/deviceQuery
-#     make -C cuda-samples/Samples/1_Utilities/bandwidthTest
-#     cuda-samples/Samples/1_Utilities/deviceQuery/deviceQuery
-#     cuda-samples/Samples/1_Utilities/bandwidthTest/bandwidthTest
-# fi
-if [[ $(should_install_dnf nvidia-container-toolkit) == "True" ]]; then
-    # https://github.com/NVIDIA/k8s-device-plugin#quick-start
-    # https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html
-
-    distribution=$(. /etc/os-release;echo centos8) \
-    && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.repo | sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
-    sudo dnf clean expire-cache
-    sudo dnf install -y nvidia-container-toolkit
-    distribution=$(. /etc/os-release;echo centos8) \
-        && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.repo | sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
-    # cat  /usr/share/containers/oci/hooks.d/oci-nvidia-hook.json
-
-    sudo dnf install -y nvidia-container-runtime nvidia-modprobe
-
-    # https://dischord.org/2022/06/14/rke2-and-nvidia-gpus-with-the-grid-operator/
-# https://dischord.org/2022/05/16/k3s-rke2-and-gpu-pci-passthrough/
-
-# grep "nvidia-container-runtime" /var/lib/rancher/rke2/agent/etc/containerd/config.toml &>/dev/null && info "GPU containerd changes already applied" && exit 0
-# awk '1;/plugins.cri.containerd]/{print "  default_runtime_name = \"nvidia-container-runtime\""}' /var/lib/rancher/rke2/agent/etc/containerd/config.toml > /var/lib/rancher/rke2/agent/etc/containerd/config.toml.tmpl
-# echo -e '\n[plugins.linux]\n  runtime = "nvidia-container-runtime"' >> /var/lib/rancher/rke2/agent/etc/containerd/config.toml.tmpl
-# echo -e '\n[plugins.cri.containerd.runtimes.nvidia-container-runtime]\n  runtime_type = "io.containerd.runc.v2"\n  [plugins.cri.containerd.runtimes.nvidia-container-runtime.options]\n    BinaryName = "nvidia-container-runtime"' >> /var/lib/rancher/rke2/agent/etc/containerd/config.toml.tmpl
-
-# DOCKER_REGISTRY_URL=$(cat defaults.json | jq -er ".registries.docker.url")
-# sed -i "s/REGISTRY_PLACEHOLDER/${DOCKER_REGISTRY_URL}/g" ./Infra_Installer/gpu_plugin/nvidia-device-plugin.yaml
-# kubectl apply -f ./Infra_Installer/gpu_plugin/nvidia-device-plugin.yaml
-# kubectl -n kube-system rollout restart daemonset nvidia-device-plugin-daemonset
-
-# root@ubuntu:~# cat /var/lib/rancher/k3s/agent/etc/containerd/config.toml | grep -i nvidia
-# [plugins.cri.containerd.runtimes."nvidia"]
-# [plugins.cri.containerd.runtimes."nvidia".options]
-#   BinaryName = "/usr/bin/nvidia-container-runtime"
-
-# kubectl get runtimeclass
-# kubectl describe node | grep gpu
-
-fi
-if [[ $(should_install_command helm) == "True" ]]; then
-    curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-fi
-if [[ $(should_install_command k3s) == "True" ]]; then
-    sudo mkdir -p /etc/rancher/k3s/
-    sudo chmod -R 777 /etc/rancher
-    cp k3s/config.yaml /etc/rancher/k3s/
-    sudo swapoff -a
-    curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--disable=traefik" INSTALL_K3S_SKIP_ENABLE=true INSTALL_K3S_SKIP_START=true sh -s - server
-    # INSTALL_K3S_VERSION="v1.23.7+k3s1" sh
-fi
-if [[ $(service_running k3s) == "False" ]]; then
-    # Set defaults
-    containerd config default | sudo tee /var/lib/rancher/k3s/agent/etc/containerd/config.toml 2>&1 > /dev/null
-    containerd config default | sudo tee /etc/containerd/config.toml 2>&1 > /dev/null
-    # Copy the template
-    sudo cp k3s/config.toml.tmpl /var/lib/rancher/k3s/agent/etc/containerd/
-    sudo cp k3s/config.toml.tmpl /etc/containerd/
-
-    sudo cp k3s/registries.yaml /etc/rancher/k3s/
-
-    sudo systemctl start k3s
-    sudo systemctl enable k3s
-    sleep 1
-    kubectl config view --raw > ~/.kube/config
-    chmod 600 ~/.kube/config
-
-    kubectl apply -f k3s/nvidia-namespace.yaml
-
-    kubectl create cm -n nvidia-device-plugin nvidia-plugin-configs \
-        --from-file=config=k3s/nvidia-config.yaml
-
-    kubectl apply -f k3s/nvidia-device-plugin.yml
-
-    label_filter="owner=joe"
-    node_name=$(kubectl get nodes -l ${label_filter} --no-headers=true -o custom-columns=NAME:.metadata.name)
-    kubectl label node --overwrite ${node_name} nvidia.com/device-plugin.config=nvidia-plugin-configs
-
-# sleep 5
-# kubectl -n kube-system create serviceaccount tiller
-# kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
-# helm init --service-account tiller
-
-#    --write-kubeconfig value, -o value         (client) Write kubeconfig for admin client to this file [$K3S_KUBECONFIG_OUTPUT]
-#    --kube-apiserver-arg value                 (flags) Customized flag for kube-apiserver process
-#    --node-name value                          (agent/node) Node name [$K3S_NODE_NAME]
-#    --container-runtime-endpoint value         (agent/runtime) Disable embedded containerd and use the CRI socket at the given path; when used with --docker this sets the docker socket path
-#    --private-registry value                   (agent/runtime) Private registry configuration file (default: "/etc/rancher/k3s/registries.yaml")
-
-
-
-# https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html#step-0-pre-requisites
-
-# 2022/12/19 07:04:53 Detected non-NVML platform: could not load NVML: libnvidia-ml.so.1: cannot open shared object file: No such file or directory
-# 2022/12/19 07:04:53 Detected non-Tegra platform: /sys/devices/soc0/family file not found
-# 2022/12/19 07:04:53 Incompatible platform detected
-# 2022/12/19 07:04:53 If this is a GPU node, did you configure the NVIDIA Container Toolkit?
-# 2022/12/19 07:04:53 You can check the prerequisites at: https://github.com/NVIDIA/k8s-device-plugin#prerequisites
-# 2022/12/19 07:04:53 You can learn how to set the runtime at: https://github.com/NVIDIA/k8s-device-plugin#quick-start
-# 2022/12/19 07:04:53 If this is not a GPU node, you should set up a toleration or nodeSelector to only deploy this plugin on GPU nodes
-# 2022/12/19 07:04:53 Error: error starting plugins: error getting plugins: unable to load resource managers to manage plugin devices: platform detection failed
-# helm repo add nvdp https://nvidia.github.io/k8s-device-plugin
-# helm repo update
-# plugin_version=$(helm search repo nvdp --devel | awk '{print $2}' | tail -1)
-# helm upgrade -i nvdp nvdp/nvidia-device-plugin \
-#   --namespace nvidia-device-plugin \
-#   --create-namespace \
-#   --version ${plugin_version} \
-#   --set config.default=nvidia-plugin-configs \
-#   --set config.name=nvidia-plugin-configs
-
-# kubectl apply -f https://raw.githubusercontent.com/kubernetes/kubernetes/release-1.14/cluster/addons/device-plugins/nvidia-gpu/daemonset.yaml
-
-
-# https://github.com/NVIDIA/k8s-device-plugin#quick-start
-# /etc/containerd/config.toml
-# /var/lib/rancher/k3s/agent/etc/containerd/config.toml
-# version = 2
-# [plugins]
-#   [plugins."io.containerd.grpc.v1.cri"]
-#     [plugins."io.containerd.grpc.v1.cri".containerd]
-#       default_runtime_name = "nvidia"
-
-#       [plugins."io.containerd.grpc.v1.cri".containerd.runtimes]
-#         [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.nvidia]
-#           privileged_without_host_devices = false
-#           runtime_engine = ""
-#           runtime_root = ""
-#           runtime_type = "io.containerd.runc.v2"
-#           [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.nvidia.options]
-#             BinaryName = "/usr/bin/nvidia-container-runtime"
-
-
-# kubectl create -f test.yaml
-
-
-
-fi
-
-
--- extra_setup.sh
+++ extra_setup.sh
@@ -6,115 +6,141 @@ add_line() {
   grep -qxF "$line" $file || echo "$line" | sudo tee -a $file
 }
 
-timedatectl set-local-rtc 1
+setup_bashrc() {
+  echo "not implemented"
+}
 
-sudo modprobe overlay
-sudo modprobe br_netfilter
-sudo modprobe ipmi_devintf
+set_clock() {
+  timedatectl set-local-rtc 1
+}
 
-cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
-overlay
-br_netfilter
+setup_containerd() {
+  sudo modprobe overlay
+  sudo modprobe br_netfilter
+  sudo modprobe ipmi_devintf
+
+  cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+  overlay
+  br_netfilter
 EOF
 
-cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
-net.bridge.bridge-nf-call-iptables  = 1
-net.ipv4.ip_forward                 = 1
-net.bridge.bridge-nf-call-ip6tables = 1
+  cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+  net.bridge.bridge-nf-call-iptables  = 1
+  net.ipv4.ip_forward                 = 1
+  net.bridge.bridge-nf-call-ip6tables = 1
 EOF
 
-sudo sysctl --system > /dev/null
+  sudo sysctl --system > /dev/null
+}
+
+setup_hosts() {
+  # TODO
+  # /etc/hosts
+  # 127.0.0.1 whoami.themachine.pc whoami
+  # 127.0.0.1 traefik.themachine.pc traefik
+  add_line '127.0.0.1 registry.themachine.pc registry' /etc/hosts
+  # add_line '192.168.1.192 registry.themachine.k3s registry' /etc/hosts
+}
+
+setup_registry_links() {
+  add_line "[[registry]]" /etc/containers/registries.conf
+  add_line 'location="registry"' /etc/containers/registries.conf
+  add_line 'insecure=true' /etc/containers/registries.conf
+}
+
+start_registry() {
+  kubectl apply -f k3s/registry-helm.yml
+}
 
 
-add_line "[[registry]]" /etc/containers/registries.conf
-add_line 'location="registry"' /etc/containers/registries.conf
-add_line 'insecure=true' /etc/containers/registries.conf
-
-# TODO
-# /etc/hosts
-# 127.0.0.1 whoami.themachine.pc whoami
-# 127.0.0.1 traefik.themachine.pc traefik
-add_line '127.0.0.1 registry.themachine.pc registry' /etc/hosts
-# add_line '192.168.1.192 registry.themachine.k3s registry' /etc/hosts
-
-
-# sudo chown ${USER} /media/ContainerStorage/storage/overlay/l
-# sudo chown ${USER} /run/user/$(id -u)/overlay
-# podman kube play  -q k3s/container-registry.yml || true
-sudo systemctl restart podman
-
-# kubectl apply -f k3s/registry-ns.yml
-# kubectl apply -f k3s/registry-sec.yml
-# kubectl apply -f k3s/registry-pvc.yml
-# kubectl apply -f k3s/registry-svc.yml
-# kubectl apply -f k3s/registry-db.yml
-# kubectl apply -f k3s/registry-helmcfg.yml
-# helm repo add twuni https://helm.twun.io
-kubectl apply -f k3s/registry-helm.yml
-# kubectl delete --all --namespace registry
-
-# TODO
-# /etc/nginx/nginx.conf
+start_services() {
+  sudo systemctl start etcd
+  sudo systemctl enable etcd
+  sudo systemctl start systemd-networkd
+  sudo systemctl enable systemd-networkd
+  sudo systemctl start sshd
+  sudo systemctl enable sshd
+  sudo systemctl start NetworkManager
+  sudo systemctl enable NetworkManager
+  # sudo systemctl start systemd-boot-check-no-failures
+  # sudo systemctl enable systemd-boot-check-no-failures
+  # sudo systemctl start nginx
+  # sudo systemctl enable nginx
+}
 
 
-# TODO
-# update unqualified-search-registries = ["registry.fedoraproject.org", "registry.access.redhat.com", "docker.io", "quay.io"]
-# update unqualified-search-registries = ["themachine.home:5000", "registry.fedoraproject.org", "registry.access.redhat.com", "docker.io", "quay.io"]
+
+other() {
+  echo other
+  # sudo chown ${USER} /media/ContainerStorage/storage/overlay/l
+  # sudo chown ${USER} /run/user/$(id -u)/overlay
+  # podman kube play  -q k3s/container-registry.yml || true
+  # sudo systemctl restart podman
+  # TODO
+  # /etc/nginx/nginx.conf
+  # TODO
+  # update unqualified-search-registries = ["registry.fedoraproject.org", "registry.access.redhat.com", "docker.io", "quay.io"]
+  # update unqualified-search-registries = ["themachine.home:5000", "registry.fedoraproject.org", "registry.access.redhat.com", "docker.io", "quay.io"]
+  # systemctl list-unit-files
+  # systemctl list-units -a
+
+  # Configure podman to use a mounted volume
+  # sudo sed -i -e '/graphroot =/ s|\(.*=\)\(.*\)|\1 "/media/ContainerStorage/storage"|' /etc/containers/storage.conf
+  # mkdir -p /home/joe/.config/containers
+  # cp /etc/containers/storage.conf /home/${USER}/.config/containers/storage.conf
+  # sudo semanage fcontext -a -t container_file_t /media/ContainerStorage/storage
+  # restorecon -R -v /media/ContainerStorage/storage
 
 
-# systemctl list-unit-files
-# systemctl list-units -a
 
-sudo systemctl start etcd
-sudo systemctl enable etcd
-sudo systemctl start systemd-networkd
-sudo systemctl enable systemd-networkd
-sudo systemctl start sshd
-sudo systemctl enable sshd
-# sudo systemctl start systemd-boot-check-no-failures
-# sudo systemctl enable systemd-boot-check-no-failures
-sudo systemctl start NetworkManager
-sudo systemctl enable NetworkManager
-sudo systemctl start nginx
-sudo systemctl enable nginx
+  # /etc/fstab
+  # LABEL=SharedData /media/SharedData ntfs-3g rw,uid=1000,gid=1000,fmask=0002,dmask=0003 0 0
+  # LABEL=SharedMedia /media/SharedMedia ntfs-3g rw,uid=1000,gid=1000,fmask=0002,dmask=0003 0 0
+  # LABEL=ContainerStorage /media/ContainerStorage ext4 default 0 0
+  # LABEL=BackupData /media/BackupData ntfs-3g rw,uid=1000,gid=1000,fmask=0002,dmask=0003 0 0
+  # LABEL=WindowsOS /media/WindowsOS ntfs-3g rw,auto,user,fmask=0002,dmask=0003 0 0
 
+  # grub2-mkconfig -o /boot/grub2/grub.cfg
+  # grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
+  # https://rootlesscontaine.rs/getting-started/common/cgroup2/
+  # To boot the host with cgroup v2, add the following string to the GRUB_CMDLINE_LINUX line in /etc/default/grub and then run sudo update-grub.
+  # systemd.unified_cgroup_hierarchy=1
 
-# grub2-mkconfig -o /boot/grub2/grub.cfg
-# grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
-# https://rootlesscontaine.rs/getting-started/common/cgroup2/
-# To boot the host with cgroup v2, add the following string to the GRUB_CMDLINE_LINUX line in /etc/default/grub and then run sudo update-grub.
-# systemd.unified_cgroup_hierarchy=1
+  # overwrite config.toml
+  # sudo mkdir -p /etc/containerd \
+  #     && sudo containerd config default | sudo tee /etc/containerd/config.toml
 
-# overwrite config.toml
-# sudo mkdir -p /etc/containerd \
-#     && sudo containerd config default | sudo tee /etc/containerd/config.toml
+  # https://dev.to/mweibel/add-nvidia-gpu-support-to-k3s-with-containerd-4j17
+  # To make use of the NVIDIA Container Runtime, additional configuration is required. The following options should be added to configure nvidia as a runtime and use systemd as the cgroup driver. A patch is provided below:
 
-# To make use of the NVIDIA Container Runtime, additional configuration is required. The following options should be added to configure nvidia as a runtime and use systemd as the cgroup driver. A patch is provided below:
+  # cat <<EOF > containerd-config.patch
+  # --- config.toml.orig    2020-12-18 18:21:41.884984894 +0000
+  # +++ /etc/containerd/config.toml 2020-12-18 18:23:38.137796223 +0000
+  # @@ -94,6 +94,15 @@
+  #         privileged_without_host_devices = false
+  #         base_runtime_spec = ""
+  #         [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
+  # +            SystemdCgroup = true
+  # +       [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.nvidia]
+  # +          privileged_without_host_devices = false
+  # +          runtime_engine = ""
+  # +          runtime_root = ""
+  # +          runtime_type = "io.containerd.runc.v1"
+  # +          [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.nvidia.options]
+  # +            BinaryName = "/usr/bin/nvidia-container-runtime"
+  # +            SystemdCgroup = true
+  #     [plugins."io.containerd.grpc.v1.cri".cni]
+  #     bin_dir = "/opt/cni/bin"
+  #     conf_dir = "/etc/cni/net.d"
+  # EOF
+  # After apply the configuration patch, restart containerd:
 
-# cat <<EOF > containerd-config.patch
-# --- config.toml.orig    2020-12-18 18:21:41.884984894 +0000
-# +++ /etc/containerd/config.toml 2020-12-18 18:23:38.137796223 +0000
-# @@ -94,6 +94,15 @@
-#         privileged_without_host_devices = false
-#         base_runtime_spec = ""
-#         [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
-# +            SystemdCgroup = true
-# +       [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.nvidia]
-# +          privileged_without_host_devices = false
-# +          runtime_engine = ""
-# +          runtime_root = ""
-# +          runtime_type = "io.containerd.runc.v1"
-# +          [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.nvidia.options]
-# +            BinaryName = "/usr/bin/nvidia-container-runtime"
-# +            SystemdCgroup = true
-#     [plugins."io.containerd.grpc.v1.cri".cni]
-#     bin_dir = "/opt/cni/bin"
-#     conf_dir = "/etc/cni/net.d"
-# EOF
-# After apply the configuration patch, restart containerd:
-
-# sudo systemctl restart containerd
+  # sudo systemctl restart containerd
 
 
-# sudo systemctl start containerd
-# sudo systemctl enable containerd
+  # sudo systemctl start containerd
+  # sudo systemctl enable containerd
+
+}
+
+
